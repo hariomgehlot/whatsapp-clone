885d38f6dc666fed5da889334898fb099e5b1040
Hariom Gehlot <hariomg530@gmail.com>
2024-01-10 16:26:54 +0000

    fix: right header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    colors:{
-      'header-gray':'#F0F2F5'
+    colors: {
+      "header-gray": "#F0F2F5",
     },
     extend: {},
-    maxHeight:{
-      '77':'77svh'
-    }
+    maxHeight: {
+      77: "77svh",
+    },
+    textColor: {
+      accent: "#6B7C85",
+    },
   },
   plugins: [],
 };
